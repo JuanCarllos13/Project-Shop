68740cb25380a5d37f955578e7a5d41c9f170107
Juan Carlos <juan2017amaral@gmail.com>
2023-03-06 10:59:04 -0400

    next structures

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title></title>
+      </Head>
+      <div>
+        <h1>TESTE PROJETO</h1>
+      </div>
+    </>
+  );
+}
